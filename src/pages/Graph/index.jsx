4b2f277510e1ef7999729a7039d4053db870c06a
josersn <nettorammos@hotmail.com>
2022-06-16 11:11:10 -0300

    start graph

---
+++ src/pages/Graph/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+import { Container } from './styles';
+
+function Graph() {
+    return (
+        <Container>
+            <Header>Gráficos de desempenho</Header>
+        </Container>
+    )
+}
+
+export default Graph;
